36a76341d541b62ad2c020031cd6a120cd8718b8
Chinthala naveen <Chinthalanaveen504@gmail.com>
2025-01-13 10:42:19 +0530

    footer done

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -15,18 +15,18 @@ function Footer() {
     </div>
     <div className="footer-section">
       <h4 style={{ color: "black" }}>Hours of Operation</h4>
-      <p>Mon - Fri: 8:00 AM - 6:00 PM</p>
-      <p>Sat - Sun: 9:00 AM - 3:00 PM</p>
+      <p style={{ color: "black" }}>Mon - Fri: 8:00 AM - 6:00 PM</p>
+      <p style={{ color: "black" }}>Sat - Sun: 9:00 AM - 3:00 PM</p>
     </div>
     <div className="footer-section">
       <h4 style={{ color: "black" }}>Address</h4>
-      <p>Plot no. 00, Road no. 0</p>
-      <p>Jubilee Hills, Hyderabad - 500000</p>
+      <p style={{ color: "black" }}>Plot no. 00, Road no. 0</p>
+      <p style={{ color: "black" }}>Jubilee Hills, Hyderabad - 500000</p>
     </div>
     <div className="footer-section">
       <h4 style={{ color: "black" }}>Contact Us</h4>
-      <p>Phone: +91 - 9000055000</p>
-      <p>Email: contact@trustcare.com</p>
+      <p style={{ color: "black" }}>Phone: +91 - 9000055000</p>
+      <p style={{ color: "black" }}>Email: contact@trustcare.com</p>
     </div>
   </footer></div>
   )
